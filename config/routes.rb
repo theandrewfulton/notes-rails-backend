32a1414e8b16181db06b7915ac51d5cdb7227450
Andrew Fulton <zenithsprings1@yahoo.com.au>
2022-02-26 20:39:27 +1100

    begin implementing devise with jwt, issues with incorrect login succeeding and no jwt in returned headers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  resources :notes
+  devise_for :users,
+              controllers: {
+                sessions: 'users/sessions',
+                registrations: 'users/registrations'
+              }
+  resources :notes, only: [:index, :show, :create, :update, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :notes, only: [:index, :show, :create, :update, :destroy]
+  # resources :notes, only: [:index, :show, :create, :update, :destroy]
 end
