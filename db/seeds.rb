2996ebbbcbaa0a2879e237b3a69737dfc4fd7ca0
Andrew Fulton <zenithsprings1@yahoo.com.au>
2022-02-27 23:41:19 +1100

    add users and modify notes in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-note1 = Note.create(title: "New Business Idea", body: "I have an amazing idea for a new business")
-note2 = Note.create(title: "To Do List", body: "take out garbage, wash dishes, do laundry")
-note3 = Note.create(title: "Shopping List", body: "bread, milk, cereal, capsicum")
+
+# Create Users
+user1 = User.create(email: "test@test.com", password: "Password123")
+user2 = User.create(email: "test@example.com", password: "differentPassword")
+
+# Create Notes
+note1 = Note.create(title: "New Business Idea", body: "I have an amazing idea for a new business", user_id: "1")
+note2 = Note.create(title: "To Do List", body: "take out garbage, wash dishes, do laundry", user_id: "1")
+note3 = Note.create(title: "Shopping List", body: "bread, milk, cereal, capsicum", user_id: "2")
