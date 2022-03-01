2c7e0322f8987af1481b15d4e9d1d6a4577c2667
Andrew Fulton <zenithsprings1@yahoo.com.au>
2022-03-01 18:00:17 +1100

    add user_id automatically to note

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -8,12 +8,23 @@ class NotesController < ApplicationController
         render json: @note
     end
     def create
-        @note = Note.create(
+        # @note = Note.create(
+        #     title: params[:title],
+        #     body: params[:body],
+        #     user_id: params[:user_id]
+        # )
+        # render json: @note
+
+        @note = @current_user.notes.new(
             title: params[:title],
-            body: params[:body],
-            user_id: params[:user_id]
+            body: params[:body]
         )
-        render json: @note
+        if @note.save
+            render json: @note, status: :created, location: @note
+        else
+            render json: @note.errors, status: :unprocessable_entity
+        end
+
     end
     def update
         @note = Note.find(params[:id])
